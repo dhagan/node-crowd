5a44bedc0f7044f1f8a0ba9fa5a23b91a8f15718
Doug Hagan <doug_hagan@sra.com>
2015-12-24 09:21:58 -1000

    create-all.js adds to group Everyone, RTOP11Admin, RTOP12Admin

--- create-all.js
+++ create-all.js
@@ -8,9 +8,13 @@ var settings = require('./settings');
 // Initialize the Crowd client:
 var crowd = new CrowdClient(settings.crowd);
 
-var users = require('./data/everyone.json');
+var everyone = require('./data/everyone.json');
+var rtop11Admin = require('./data/rtop11-admins.json');
+var rtop12Admin = require('./data/rtop12-admins.json');
 var groups = require('./data/groups.json');
-var groupName = 'Everyone';
+var groupNameEveryone = 'Everyone';
+var groupName11 = 'RTOP11Admin';
+var groupName12 = 'RTOP12Admin';
 
 async.series([
     function(callback) {
@@ -21,11 +25,17 @@ async.series([
     },
     function(callback) {
         addUsersToGroup(callback);
+    },
+    function(callback) {
+        addUsersToGroup11(callback);
+    },
+    function(callback) {
+        addUsersToGroup12(callback);
     }
 ]);
 
 function createUsers(theCallBack) {
-    async.eachSeries(users, function iterator(item, callback) {
+    async.eachSeries(everyone, function iterator(item, callback) {
             // Create a new user:
             crowd.user.create(new User(item.firstname, item.lastname, item.displayname, item.email, item.name, item.password.value)).then(function (user) {
                 console.log(user);
@@ -66,9 +76,9 @@ function createGroups(theCallBack) {
 }
 
 function addUsersToGroup(theCallBack) {
-    async.eachSeries(users, function iterator(item, callback) {
+    async.eachSeries(everyone, function iterator(item, callback) {
             // add user to group:
-            crowd.group.users.add(groupName, item.name).then(function (user) {
+            crowd.group.users.add(groupNameEveryone, item.name).then(function (user) {
                 console.log('success');
                 callback(null);
             }, function (error) {
@@ -78,8 +88,48 @@ function addUsersToGroup(theCallBack) {
         },
         function done() {
             // Find all active groups (using Crowd Query Language):
-            crowd.group.users.list(groupName).then(function (members) {
-                console.log('Group: ' + groupName + ' - membership: ' + members);
+            crowd.group.users.list(groupNameEveryone).then(function (members) {
+                console.log('Group: ' + groupNameEveryone + ' - membership: ' + members);
+                theCallBack();
+            });
+        })
+}
+
+function addUsersToGroup11(theCallBack) {
+    async.eachSeries(rtop11Admin, function iterator(item, callback) {
+            // add user to group:
+            crowd.group.users.add(groupName11, item.name).then(function (user) {
+                console.log('success');
+                callback(null);
+            }, function (error) {
+                console.log(error);
+                callback(null);
+            });
+        },
+        function done() {
+            // Find all active groups (using Crowd Query Language):
+            crowd.group.users.list(groupName11).then(function (members) {
+                console.log('Group: ' + groupName11 + ' - membership: ' + members);
+                theCallBack();
+            });
+        })
+}
+
+function addUsersToGroup12(theCallBack) {
+    async.eachSeries(rtop12Admin, function iterator(item, callback) {
+            // add user to group:
+            crowd.group.users.add(groupName12, item.name).then(function (user) {
+                console.log('success');
+                callback(null);
+            }, function (error) {
+                console.log(error);
+                callback(null);
+            });
+        },
+        function done() {
+            // Find all active groups (using Crowd Query Language):
+            crowd.group.users.list(groupName12).then(function (members) {
+                console.log('Group: ' + groupName12 + ' - membership: ' + members);
                 theCallBack();
             });
         })
